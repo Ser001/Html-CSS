8bff144062d3a42b9483116c8824675abbc34d61
Sérgio <sergiojunior20016@hotmail.com>
2023-09-25 12:08:29 -0300

    atualizações efeito parallax

---
+++ Exercicios/EfeitoParallax/script.js
@@ -0,0 +1,10 @@
+
+window.sr = ScrollReveal({reset: true});
+
+sr.reveal('.area1',{ duration:1000});
+sr.reveal('.area2',{
+    rotate:{ x:0, y:80, z:0},
+        duration:2500});
+sr.reveal('.area3',{ 
+    rotate:{x:100, y:100, z:0},
+    duration:2000});
